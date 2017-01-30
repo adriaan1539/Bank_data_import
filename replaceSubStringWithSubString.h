95bde31fd0bf19cf1567f21c5b0117ef4471f589
Adriaan <adriaan.s@gmail.com>
2017-01-30 17:08:36 +0100

    Add function 'replaceSubStringWithSubString'.

---
+++ replaceSubStringWithSubString.h
@@ -0,0 +1,7 @@
+#ifndef REPLACESUBSTRINGWITHSUBSTRING_H
+#define REPLACESUBSTRINGWITHSUBSTRING_H
+
+#include <string>
+
+std::string replaceSubStringWithSubString(std::string str,const std::string& from,const std::string& to);
+#endif
