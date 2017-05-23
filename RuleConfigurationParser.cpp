d65b2d641e03461f04580c5194731e6ec876078a
Terry Seidler <terry.seidler@ogd.nl>
2017-05-23 21:34:16 +0200

    Set default ruleset name in rules.xml

--- RuleConfigurationParser.cpp
+++ RuleConfigurationParser.cpp
@@ -8,5 +8,5 @@ RuleConfigurationParser::RuleConfigurationParser() {
 void RuleConfigurationParser::LoadRulesFromFile(std::string filename) {
 	pugi::xml_parse_result result = this->configurationFile.load_file(filename.c_str());
 
-	std::cout << "Rule Configuration load result: " << result.description() << ", mesh name: " << this->configurationFile.child("mesh").attribute("name").value() << std::endl;
+	std::cout << "Rule Configuration load result: " << result.description() << ", ruleset name: " << this->configurationFile.child("rules").attribute("name").value() << std::endl;
 }
